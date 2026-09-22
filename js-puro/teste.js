faf59f723cd7d6eb57b10681d409fc95dfc41734
Igor Medeiros Freitas <Igor57780127>
2026-09-22 18:07:50 -0300

    mamãozinho

---
+++ js-puro/teste.js
@@ -0,0 +1,21 @@
+const cursos = [
+    {
+        nome: "React",
+        cargaHoraria: 112
+    },
+    {
+        nome: "JavaScript",
+        cargaHoraria: 80
+    },
+    {
+        nome: "Node.js",
+        cargaHoraria: 60
+    }
+];
+
+console.log(cursos[0].nome);
+console.log(cursos[0].cargaHoraria);
+console.log(cursos[1].nome);
+console.log(cursos[2].nome);
+
+console.log(cursos.length)
